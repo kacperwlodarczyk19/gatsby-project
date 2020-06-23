6816c4436be9df26ab4d191e8b6d6f04725bbbff
Kacper Włodarczyk <60901679+kacperwlodarczyk19@users.noreply.github.com>
2020-06-23 20:59:23 +0200

    Navbar changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,9 +5,12 @@ import { useStaticQuery, graphql } from "gatsby"
 import "./bootstrap.min.css"
 
 import "./layout.css"
+import Navbar from './Globals/Navbar'
+
 
 const Layout = ({ children }) => 
   <>
+    <Navbar />
     {children}
   </>
   
