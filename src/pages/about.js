24c2a64fc6de18b424ffbf144c20281547086856
Kacper Włodarczyk <60901679+kacperwlodarczyk19@users.noreply.github.com>
2020-06-24 18:38:47 +0200

    Connected to Contenful

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,12 +8,13 @@ import BackgroundSection from '../components/Globals/BackgroundSection'
 import Info from '../components/Home/Info'
 
 
-const IndexPage = ( {data} ) => (
+const AboutPage = ( {data} ) => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="About" />
     <BackgroundSection 
       img={data.img.childImageSharp.fluid} 
-      title="regular joe's"
+      title="about us"
+      styleClass="about-background"
     >
     </BackgroundSection>
     <Info />
@@ -22,7 +23,7 @@ const IndexPage = ( {data} ) => (
 
 export const query = graphql`
   {
-    img: file(relativePath: {eq: "default-background.jpeg"}) {
+    img: file(relativePath: {eq: "about-background.jpeg"}) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -36,4 +37,4 @@ export const query = graphql`
 
 
 
-export default IndexPage
+export default AboutPage
