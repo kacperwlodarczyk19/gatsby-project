fe12a027b91ba035a924f2999e46deee1119e11a
Kacper Włodarczyk <60901679+kacperwlodarczyk19@users.noreply.github.com>
2020-06-24 19:25:34 +0200

    Bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,7 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `process.env.CONTENTFUL_SPACE_ID`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
